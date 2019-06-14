7b87cf4806686d5cdd143800f7b252c1c7cfca31
aj <ajaguasin@gmail.com>
2019-06-13 23:37:45 -0700

    Finish 6. HTTP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HeroesComponent } from "./components/heroes/heroes.component";
 import { HeroDetailComponent } from './components/heroes/hero-detail/hero-detail.component';
 import { MessagesComponent } from './components/messages/messages.component';
-import { AppRoutingModule } from './app-routing.module';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+import { HeroSearchComponent } from './components/hero-search/hero-search.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent, DashboardComponent],
-  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent, DashboardComponent, HeroSearchComponent],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule, HttpClientInMemoryWebApiModule.forRoot(
+    InMemoryDataService, { dataEncapsulation: false }
+  )],
   providers: [],
   bootstrap: [AppComponent]
 })
